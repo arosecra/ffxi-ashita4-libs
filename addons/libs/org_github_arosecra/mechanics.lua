e229ebf920bfcf8ffe95d90703d6fadfb905869c
arosecra <34662069+arosecra@users.noreply.github.com>
2021-12-23 21:32:57 -0600

    added mechanics, ordered pair implementation

--- addons/libs/org_github_arosecra/mechanics.lua
+++ addons/libs/org_github_arosecra/mechanics.lua
@@ -17,4 +17,36 @@ mechanics.health_percent_to_status = function(percentage)
 	return 'dead';
 end
 
+mechanics.is_ability_waiting_recast = function(ability_id)
+	local result = false
+	for i = 0, 31 do
+		local id = AshitaCore:GetMemoryManager():GetRecast():GetAbilityTimerId(i);
+		local timer = AshitaCore:GetMemoryManager():GetRecast():GetAbilityTimer(i);
+		
+		if ((id ~= 0 or i == 0) and timer > 0 and id == ability_id) then
+			result = true
+		end
+	end	
+	return result
+end
+
+mechanics.is_spell_waiting_recast = function(spell_id)
+	local timer = AshitaCore:GetMemoryManager():GetRecast():GetSpellTimer(spell_id);
+	return timer > 0
+end
+
+mechanics.is_status_active = function(status_id)
+	local applied = false
+	local status_list = AshitaCore:GetMemoryManager():GetPlayer():GetStatusIcons();
+	for slot = 1, 32 do
+		if status_list[slot] ~= nil then
+			local buffid = status_list[slot]
+			if buffid == status_id then
+				applied = true
+			end
+		end
+	end
+	return applied
+end
+
 return mechanics;
